904c01e67f61a3a304669ade18d42c8b68be0d26
Marcos Alberto Cosmoski Filho <marcosmoski@Marcoss-MacBook-Air.local>
2024-02-12 19:00:51 -0300

    objeects

--- arrays.ts
+++ arrays.ts
@@ -1,5 +1,5 @@
 {
-  const string[] = ["aaa", "bbb"]
+  const string: string[] = ["aaa", "bbb"]
   const numbers: Array<Number> = [1,2,3]
   const matrix: Array<Array<string>> = [['aaaa', 'aaa'], ['1234']]
 }
---
+++ objects.ts
@@ -0,0 +1,30 @@
+{
+  type Pessoa = { 
+    nome: string
+    idade: number
+    corFavorita: string
+  }
+
+  const pessoa: Pessoa = {
+    nome: "aaa", 
+    idade: 12, 
+    corFavorita: "cor"
+  }
+
+  const ObjetoLiteralPessoa: {nome: string, idade: number, corFavorita: string} = {
+    nome: "aaa", 
+    idade: 12, 
+    corFavorita: "cor"
+  }
+
+  function foo(pessoa: Pessoa) {}
+
+  type Carro = { 
+    cor: string, 
+    dono: Pessoa,
+    revisoes: Array<string>
+  }
+
+  
+
+}
